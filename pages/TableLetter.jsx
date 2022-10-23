2dfe0493cd4d365542faac4ae9c671aad3abe058
Martyna1202 <martina.mischlinski@gmail.com>
2022-10-23 17:38:04 +0200

    flight widget migrated to next.js

---
+++ pages/TableLetter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+
+const TableLetter = ({ letter, index }) => {
+  const [flip, setFlip] = useState(false);
+
+  setTimeout(() => {
+    setFlip(true);
+  }, 100 * index);
+
+  return (
+    <div className={flip ? styles.flip : null}>{flip ? letter : null}</div>
+  );
+};
+export default TableLetter;
